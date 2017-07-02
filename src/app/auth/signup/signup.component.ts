d77c2f04259610cd52ade8af10e9c89b936b425d
kokyhin <kokyhin@gmail.com>
2017-07-02 18:28:52 +0300

    Add signup validation

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email, this.checkEmail]),
+      'password': new FormControl(null, [Validators.required]),
+      'passwordCheck': new FormControl(null, [Validators.required, this.checkPassword.bind(this)], )
+    });
   }
 
+  checkPassword(control: FormControl): {[s: string]: boolean} {
+    if (!this.signupForm) {return; }
+    const password = this.signupForm.get('password').value;
+    const passCheck = control.value;
+    if (password !== passCheck) {
+      return {'passRepeatFail': true};
+    }
+    return null;
+  }
+
+  checkEmail(control: FormControl): {[s: string]: boolean} {
+    if (control.value) {
+      const mailDomen = control.value.split('@')[1];
+      if (mailDomen !== 'fusionworks.md') {
+        return {'notCorporateEmail': true};
+      }
+    }
+    return null;
+  }
+
+  onSubmit() {
+    console.log('Form Submitted');
+  }
 }
