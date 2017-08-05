c72739b9a23c344f8658871c89dc0390d5c06e03
kokyhin <kokyhin@gmail.com>
2017-08-05 12:24:11 +0300

    Order model new structure

--- server/models/order.js
+++ server/models/order.js
@@ -3,7 +3,8 @@ var Schema   = mongoose.Schema;
 
 var order = new Schema({
   id: Schema.ObjectId,
-  date: Number,
+  date: String,
+  unix: Date,
   first: {
     value: {type: Number, default: 0},
     option: {type: String, default: 'default'}
