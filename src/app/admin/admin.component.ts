a58b7fcdfebf2458c726598620164b0ce2a71bea
kokyhin <kokyhin@gmail.com>
2017-08-22 12:25:07 +0300

    DatePicker moved to a separate component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,10 +1,8 @@
 import { AuthService } from './../auth/auth.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NotificationsService } from 'angular2-notifications';
 import { AdminService } from './admin.service';
 import { Component, OnInit } from '@angular/core';
 import { Response } from '@angular/http';
-import {IMyDpOptions} from 'mydatepicker';
 
 @Component({
   selector: 'app-admin',
@@ -12,27 +10,15 @@ import {IMyDpOptions} from 'mydatepicker';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  myDatePickerOptions: IMyDpOptions = {
-    dateFormat: 'dd.mm.yyyy',
-    markCurrentDay: true,
-    satHighlight: true,
-    disableWeekends: true
-  };
 
-  myForm: FormGroup;
   constructor(
     private adminService: AdminService,
     private notify: NotificationsService,
-    private formBuilder: FormBuilder,
     private authService: AuthService,
   ) { }
 
   ngOnInit() {
     this.getWeek();
-    this.myForm = this.formBuilder.group({
-      myDate: [null, Validators.required]
-    });
-    this.setDate();
   }
 
   getWeek() {
@@ -40,20 +26,6 @@ export class AdminComponent implements OnInit {
       (response: Response) => { console.log(response.json()); },
       (error) => { this.notify.error(error.json().message); }
     );
-  }
-
-  setDate(): void {
-    const date = new Date();
-    this.myForm.patchValue({myDate: {
-    date: {
-      year: date.getFullYear(),
-      month: date.getMonth() + 1,
-      day: date.getDate()}
-    }});
-  }
-
-  clearDate(): void {
-    this.myForm.patchValue({myDate: null});
   }
 
   logout() {
--- src/app/admin/custom-meal/custom-meal.component.ts
+++ src/app/admin/custom-meal/custom-meal.component.ts
@@ -1,15 +1,40 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-
+import {IMyDpOptions} from 'mydatepicker';
 @Component({
   selector: 'app-custom-meal',
   templateUrl: './custom-meal.component.html',
   styleUrls: ['./custom-meal.component.scss']
 })
 export class CustomMealComponent implements OnInit {
-
-  constructor() { }
+  // formBuilder: any;
+  myDatePickerOptions: IMyDpOptions = {
+    dateFormat: 'dd.mm.yyyy',
+    markCurrentDay: true,
+    satHighlight: true,
+    disableWeekends: true
+  };
+  myForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      myDate: [null, Validators.required]
+    });
+    this.setDate();
   }
 
+  setDate(): void {
+    const date = new Date();
+    this.myForm.patchValue({myDate: {
+    date: {
+      year: date.getFullYear(),
+      month: date.getMonth() + 1,
+      day: date.getDate()}
+    }});
+  }
+
+  clearDate(): void {
+    this.myForm.patchValue({myDate: null});
+  }
 }
